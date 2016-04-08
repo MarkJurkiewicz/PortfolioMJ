0353270325a85d94075066ef3298815c10e71190
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-04-07 18:17:27 -0700

    test

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require_once('email_config.php');
+//require_once('email_config.php');
 require('PHPMailerAutoload.php');
 $mail = new PHPMailer();
 $mail->SMTPDebug = 3;                               // Enable verbose debug output
@@ -11,8 +11,8 @@ $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 
 
-$mail->Username = EMAIL_USER;                 // SMTP username
-$mail->Password = EMAIL_PASS;                 // SMTP password
+$mail->Username = 'wigglermaster@gmail.com';                  // SMTP username
+$mail->Password = 'andrew33';                 // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 $options = array(
