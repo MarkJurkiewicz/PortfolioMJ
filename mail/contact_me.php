7c4cd2f91dd692278925e4836b93f94c56fb0e88
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-04-07 18:24:07 -0700

    testing gitignore

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,8 +11,8 @@ $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 
 
-$mail->Username = 'wigglermaster@gmail.com';                  // SMTP username
-$mail->Password = 'andrew33';                 // SMTP password
+$mail->Username = EMAIL_USER;                  // SMTP username
+$mail->Password = EMAIL_PASS;                 // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 $options = array(
